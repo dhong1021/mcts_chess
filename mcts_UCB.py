2ae11334c5b667f24ea0ab2b864493b3957494f1
dhong1021 <59321922+dhong1021@users.noreply.github.com>
2022-06-03 11:20:48 +0900

    Add files via upload
    None이 나오지 않도록 수정
    ucb 계산식 수정?

---
+++ mcts_UCB.py
@@ -0,0 +1,355 @@
+import chess
+import random
+from math import log,sqrt,e,inf
+import time
+
+class node():
+    def __init__(self):
+        self.state = chess.Board()
+        self.action = ''
+        self.children = set()
+        self.parent = None
+        self.N = 0
+        self.n = 0
+        self.v = 0
+        self.win = 0
+
+def ucb1(curr_node):
+    #ans = curr_node.v + 2 * (sqrt(log(curr_node.N + e + (10**-6))/(curr_node.n + (10**-10))))
+    c = 0
+    if curr_node.N == 0:
+        c = 1
+    ans = winrate(curr_node) + sqrt(2) * sqrt(log(curr_node.N + c + (10**-6)) / (curr_node.n + (10**-10)))
+    return ans
+
+def winrate(curr_node):
+    if(curr_node.n == 0):
+        return 1
+    else:
+        return curr_node.win / curr_node.n
+
+def rollout(curr_node):
+    if(chess.Board.outcome(curr_node.state) != None):
+        board = curr_node.state
+        if(board.result()=='1-0'):
+            return (1,curr_node)
+        elif(board.result()=='0-1'):
+            return (-1,curr_node)
+        else:
+            return (0.5,curr_node)
+    
+    all_moves = [curr_node.state.san(i) for i in list(curr_node.state.legal_moves)]
+    
+    for i in all_moves:
+        tmp_state = chess.Board(curr_node.state.fen())
+        tmp_state.push_san(i)
+        child = node()
+        child.state = tmp_state
+        child.parent = curr_node
+        curr_node.children.add(child)
+    rnd_state = random.choice(list(curr_node.children))
+
+    return rollout(rnd_state)
+
+def expand(curr_node,white):
+    if(len(curr_node.children)==0):
+        return curr_node
+    max_ucb = -inf
+    if(white):
+        idx = -1
+        max_ucb = -inf
+        sel_child = None
+        for i in curr_node.children:
+            tmp = ucb1(i)
+            if(tmp>max_ucb):
+                idx = i
+                max_ucb = tmp
+                sel_child = i
+
+        return(expand(sel_child,0))
+
+    else:
+        idx = -1
+        min_ucb = inf
+        sel_child = None
+        for i in curr_node.children:
+            tmp = ucb1(i)
+            if(tmp<min_ucb):
+                idx = i
+                min_ucb = tmp
+                sel_child = i
+
+        return expand(sel_child,1)
+
+def rollback(curr_node,reward):
+    while(curr_node.parent!=None):
+        curr_node.n+=1
+        curr_node.v+=reward
+        curr_node.N+=1
+        if (reward == 1):
+            curr_node.win += 1
+        curr_node = curr_node.parent
+    return curr_node
+
+def mcts_pred(curr_node,over,white,iterations=2):
+    if(over != None):
+        return -1
+        
+    while(iterations>0):
+        if(white):
+            idx = -1
+            max_ucb = -inf
+            sel_child = None
+            for i in curr_node.children:
+                tmp = ucb1(i)
+                if(tmp>max_ucb):
+                    idx = i
+                    max_ucb = tmp
+                    sel_child = i
+            ex_child = expand(sel_child,0)
+            reward,state = rollout(ex_child)
+            curr_node = rollback(state,reward)
+            iterations-=1
+        else:
+            idx = -1
+            min_ucb = inf
+            sel_child = None
+            for i in curr_node.children:
+                tmp = ucb1(i)
+                if(tmp<min_ucb):
+                    idx = i
+                    min_ucb = tmp
+                    sel_child = i
+
+            ex_child = expand(sel_child,1)
+
+            reward,state = rollout(ex_child)
+
+            curr_node = rollback(state,reward)
+            iterations-=1
+    if(white):
+        
+        mx = -inf
+        idx = -1
+        selected_move = ''
+        for i in (curr_node.children):
+            tmp = ucb1(i)
+            if(tmp>mx):
+                mx = tmp
+                selected_move = map_state_move[i]
+        return selected_move
+    else:
+        mn = inf
+        idx = -1
+        selected_move = ''
+        for i in (curr_node.children):
+            tmp = ucb1(i)
+            if(tmp<mn):
+                mn = tmp
+                selected_move = map_state_move[i]
+        return selected_move
+    
+def evaluate_board(): 
+    
+    if board.is_checkmate():  
+        if board.turn:
+            return 9999  
+        else:
+            return -9999  
+    if board.is_stalemate():  
+        return 0
+    if board.is_insufficient_material():
+        return 0
+    
+    wp = len(board.pieces(chess.PAWN, chess.WHITE))
+    bp = len(board.pieces(chess.PAWN, chess.BLACK))
+    wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
+    bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
+    wb = len(board.pieces(chess.BISHOP, chess.WHITE))
+    bb = len(board.pieces(chess.BISHOP, chess.BLACK))
+    wr = len(board.pieces(chess.ROOK, chess.WHITE))
+    br = len(board.pieces(chess.ROOK, chess.BLACK))
+    wq = len(board.pieces(chess.QUEEN, chess.WHITE))
+    bq = len(board.pieces(chess.QUEEN, chess.BLACK))
+    
+    material = 100*(wp-bp)+320*(wn-bn)+330*(wb-bb)+500*(wr-br)+900*(wq-bq)  
+    
+    pawnsq = sum([pawntable[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
+    pawnsq= pawnsq + sum([-pawntable[chess.square_mirror(i)] 
+                                    for i in board.pieces(chess.PAWN, chess.BLACK)])
+    knightsq = sum([knightstable[i] for i in board.pieces(chess.KNIGHT, chess.WHITE)])
+    knightsq = knightsq + sum([-knightstable[chess.square_mirror(i)] 
+                                    for i in board.pieces(chess.KNIGHT, chess.BLACK)])
+    bishopsq= sum([bishopstable[i] for i in board.pieces(chess.BISHOP, chess.WHITE)])
+    bishopsq= bishopsq + sum([-bishopstable[chess.square_mirror(i)] 
+                                    for i in board.pieces(chess.BISHOP, chess.BLACK)])
+    rooksq = sum([rookstable[i] for i in board.pieces(chess.ROOK, chess.WHITE)]) 
+    rooksq = rooksq + sum([-rookstable[chess.square_mirror(i)] 
+                                    for i in board.pieces(chess.ROOK, chess.BLACK)])
+    queensq = sum([queenstable[i] for i in board.pieces(chess.QUEEN, chess.WHITE)]) 
+    queensq = queensq + sum([-queenstable[chess.square_mirror(i)] 
+                                    for i in board.pieces(chess.QUEEN, chess.BLACK)])
+    kingsq = sum([kingstable[i] for i in board.pieces(chess.KING, chess.WHITE)]) 
+    kingsq = kingsq + sum([-kingstable[chess.square_mirror(i)] 
+                                    for i in board.pieces(chess.KING, chess.BLACK)])
+    
+    eval = material + pawnsq + knightsq + bishopsq+ rooksq+ queensq + kingsq
+    if board.turn:
+        return -eval  
+    else:
+        return eval  
+    
+pawntable = [
+ 0,  0,  0,  0,  0,  0,  0,  0,
+ 5, 10, 10,-20,-20, 10, 10,  5,
+ 5, -5,-10,  0,  0,-10, -5,  5,
+ 0,  0,  0, 20, 20,  0,  0,  0,
+ 5,  5, 10, 25, 25, 10,  5,  5,
+10, 10, 20, 30, 30, 20, 10, 10,
+50, 50, 50, 50, 50, 50, 50, 50,
+ 0,  0,  0,  0,  0,  0,  0,  0]
+
+knightstable = [
+-50,-40,-30,-30,-30,-30,-40,-50,
+-40,-20,  0,  5,  5,  0,-20,-40,
+-30,  5, 10, 15, 15, 10,  5,-30,
+-30,  0, 15, 20, 20, 15,  0,-30,
+-30,  5, 15, 20, 20, 15,  5,-30,
+-30,  0, 10, 15, 15, 10,  0,-30,
+-40,-20,  0,  0,  0,  0,-20,-40,
+-50,-40,-30,-30,-30,-30,-40,-50]
+
+bishopstable = [
+-20,-10,-10,-10,-10,-10,-10,-20,
+-10,  5,  0,  0,  0,  0,  5,-10,
+-10, 10, 10, 10, 10, 10, 10,-10,
+-10,  0, 10, 10, 10, 10,  0,-10,
+-10,  5,  5, 10, 10,  5,  5,-10,
+-10,  0,  5, 10, 10,  5,  0,-10,
+-10,  0,  0,  0,  0,  0,  0,-10,
+-20,-10,-10,-10,-10,-10,-10,-20]
+
+rookstable = [
+  0,  0,  0,  5,  5,  0,  0,  0,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+  5, 10, 10, 10, 10, 10, 10,  5,
+ 0,  0,  0,  0,  0,  0,  0,  0]
+
+queenstable = [
+-20,-10,-10, -5, -5,-10,-10,-20,
+-10,  0,  0,  0,  0,  0,  0,-10,
+-10,  5,  5,  5,  5,  5,  0,-10,
+  0,  0,  5,  5,  5,  5,  0, -5,
+ -5,  0,  5,  5,  5,  5,  0, -5,
+-10,  0,  5,  5,  5,  5,  0,-10,
+-10,  0,  0,  0,  0,  0,  0,-10,
+-20,-10,-10, -5, -5,-10,-10,-20]
+
+kingstable = [
+ 20, 30, 10,  0,  0, 10, 30, 20,
+ 20, 20,  0,  0,  0,  0, 20, 20,
+-10,-20,-20,-20,-20,-20,-20,-10,
+-20,-30,-30,-40,-40,-30,-30,-20,
+-30,-40,-40,-50,-50,-40,-40,-30,
+-30,-40,-40,-50,-50,-40,-40,-30,
+-30,-40,-40,-50,-50,-40,-40,-30,
+-30,-40,-40,-50,-50,-40,-40,-30]
+
+def alphabeta( alpha, beta, depthleft ):  
+    bestscore = -9999
+    if( depthleft == 0 ): 
+        return quiesce( alpha, beta )
+    for move in board.legal_moves: 
+        board.push(move)   
+        score = -alphabeta( -beta, -alpha, depthleft - 1 )
+        board.pop() 
+        if( score >= beta ): 
+            return score
+        if( score > bestscore ):
+            bestscore = score
+        if( score > alpha ):
+            alpha = score   
+    return bestscore 
+
+def quiesce( alpha, beta ):
+    stand_pat = evaluate_board() 
+    if( stand_pat >= beta ):
+        return beta
+    if( alpha < stand_pat ):
+        alpha = stand_pat
+
+    for move in board.legal_moves:
+        if board.is_capture(move):
+            board.push(move)        
+            score = -quiesce( -beta, -alpha )
+            board.pop()
+
+            if( score >= beta ):
+                return beta
+            if( score > alpha ):
+                alpha = score  
+    return alpha
+
+
+def selectmove(depth):
+    bestMove = chess.Move.null()
+    bestValue = -99999
+    alpha = -100000
+    beta = 100000
+    for move in board.legal_moves: 
+        board.push(move) 
+        boardValue = -alphabeta(-beta, -alpha, depth-1)
+        if boardValue > bestValue:
+            bestValue = boardValue;
+            bestMove = move 
+        if( boardValue > alpha ):
+            alpha = boardValue 
+        board.pop() 
+    return bestMove
+board = chess.Board()
+
+start_time = time.time()
+white = 1
+#over_condition = board.is_game_over() or board.can_claim_draw()
+while (chess.Board.outcome(board) is None):
+    if board.turn:
+        print('MCTS')
+        root = node()
+        root.state = board
+        all_moves = [root.state.san(i) for i in list(root.state.legal_moves)]
+        map_state_move = dict()
+    
+        for i in all_moves:
+            tmp_state = chess.Board(root.state.fen())
+            tmp_state.push_san(i)
+            child = node()
+            child.state = tmp_state
+            child.parent = root
+            root.children.add(child)
+            map_state_move[child] = i
+            
+        result = mcts_pred(root,chess.Board.outcome(board),white, 3)
+        board.push_san(result)
+        print(result)
+        print(board)
+        print()
+    else:
+        print('MMAB')
+        move = selectmove(3)
+        board.push(move)
+        print(move)
+        print(board)
+        print()
+
+#print(board.result())
+print(chess.Board.outcome(board))
+if board.is_fifty_moves():
+    print("fifty moves")
+end_time = time.time()
+print(end_time - start_time, ' sec')
+
+#python3 mcts_ucb.py
