ebde0f5fe5095cb0af8681d17ae6460e647f0efd
Sangwoo <100983869+woorang@users.noreply.github.com>
2022-06-13 20:49:20 +0900

    [FEAT] #8 - add game play

--- UCB MCTS.py
+++ UCB MCTS.py
@@ -23,11 +23,11 @@ def ucb(cur_node):
     ucb_value = winrate(cur_node) + c * sqrt(log(cur_node.t + e + (10**-6)) / (cur_node.n + (10**-10)))
     return ucb_value
     
-def expand(cur_node, white):
+def ucb_expand(cur_node, white_turn):
     if len(cur_node.children) == 0:
         return cur_node
     
-    if(white):
+    if white_turn:
         max_ucb = -inf
         selected_child = None
         for child in cur_node.children:
@@ -35,7 +35,7 @@ def expand(cur_node, white):
             if temp > max_ucb:
                 max_ucb = temp
                 selected_child = child
-        return expand(selected_child, 0)
+        return ucb_expand(selected_child, False)
     
     else:
         min_ucb = inf
@@ -45,9 +45,9 @@ def expand(cur_node, white):
             if temp < min_ucb:
                 min_ucb = temp
                 selected_child = child
-        return expand(selected_child, 1)
+        return ucb_expand(selected_child, True)
     
-def UCB_rollout(cur_node):
+def ucb_rollout(cur_node):
     if chess.Board.outcome(cur_node.state) != None:
         board = cur_node.state
         if board.result() == '1-0':
@@ -57,18 +57,18 @@ def UCB_rollout(cur_node):
         else:
             return (0.5, cur_node)
     
-    all_moves = [cur_node.state.san(i) for i in list(cur_node.state.legal_moves)]
+    all_moves = [cur_node.state.san(move) for move in list(cur_node.state.legal_moves)]
     
-    for i in all_moves:
+    for move in all_moves:
         temp_state = chess.Board(cur_node.state.fen())
-        temp_state.push_san(i)
+        temp_state.push_san(move)
         child = node()
         child.state = temp_state
         child.parent = cur_node
         cur_node.children.add(child)
     random_state = random.choice(list(cur_node.children))
 
-    return UCB_rollout(random_state)
+    return ucb_rollout(random_state)
 
 def rollback(cur_node, reward):
     while cur_node.parent != None:
@@ -80,63 +80,79 @@ def rollback(cur_node, reward):
         cur_node = cur_node.parent
     return cur_node
 
-def mcts_UCB(cur_node, over, white, iterations):
+def ucb_mcts(cur_node, over, white_turn, iterations):
     if(over != None):
         return -1
     
     all_moves = [root.state.san(move) for move in list(root.state.legal_moves)]
     state_moves = dict()
 
-    for i in all_moves:
+    for move in all_moves:
         temp_state = chess.Board(root.state.fen())
-        temp_state.push_san(i)
+        temp_state.push_san(move)
         child = node()
         child.state = temp_state
         child.parent = root
         root.children.add(child)
-        state_moves[child] = i
+        state_moves[child] = move
         
     while iterations > 0:
-        if white:
+        if white_turn:
             max_ucb = -inf
             selected_child = None
-            for i in cur_node.children:
-                temp = ucb(i)
+            for child in cur_node.children:
+                temp = ucb(child)
                 if temp > max_ucb:
                     max_ucb = temp
-                    selected_child = i
-            expand_child = expand(selected_child, 0)
-            reward,state = UCB_rollout(expand_child)
+                    selected_child = child
+            expand_child = ucb_expand(selected_child, False)
+            reward,state = ucb_rollout(expand_child)
             cur_node = rollback(state, reward)
             iterations -= 1
         else:
             min_ucb = inf
             selected_child = None
-            for i in cur_node.children:
-                temp = ucb(i)
+            for child in cur_node.children:
+                temp = ucb(child)
                 if temp < min_ucb:
                     min_ucb = temp
-                    selected_child = i
-            expand_child = expand(selected_child, 1)
-            reward,state = UCB_rollout(expand_child)
+                    selected_child = child
+            expand_child = ucb_expand(selected_child, True)
+            reward,state = ucb_rollout(expand_child)
             cur_node = rollback(state, reward)
             iterations -= 1
             
-    if white:
+    if white_turn:
         mx = -inf
         selected_move = ''
-        for j in cur_node.children:
-            temp = winrate(j)
+        for child in cur_node.children:
+            temp = winrate(child)
             if temp > mx:
                 mx = temp
-                selected_move = state_moves[j]
+                selected_move = state_moves[child]
         return selected_move
     else:
         mn = inf
         selected_move = ''
-        for j in cur_node.children:
-            temp = winrate(j)
+        for child in cur_node.children:
+            temp = winrate(child)
             if temp < mn:
                 mn = temp
-                selected_move = state_moves[j]
+                selected_move = state_moves[child]
         return selected_move
+
+board = chess.Board()
+while chess.Board.outcome(board) is None:
+    if board.turn:
+        root = node()
+        root.state = board
+        next_move = ucb_mcts(root, chess.Board.outcome(board), board.turn, 5)
+        board.push_san(next_move)
+        print(board)
+    else:
+        root = node()
+        root.state = board
+        next_move = ucb_mcts(root, chess.Board.outcome(board), board.turn, 5)
+        board.push_san(next_move)
+        print(board)
+print(chess.Board.outcome(board))
